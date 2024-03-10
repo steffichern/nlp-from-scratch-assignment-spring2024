a9b7278dea6b5ff3efcf02a43aec73448efdbad3
Shuning Lin <shuningl@andrew.cmu.edu>
2024-03-10 12:36:50 -0400

    can run all in local!

--- python_setup.sh
+++ python_setup.sh
@@ -1,3 +1,4 @@
+# Run these commands
 pip install llama-index
 pip install llama-index-vector-stores-postgres
 pip install InstructorEmbedding
@@ -5,3 +6,5 @@ pip install -U sentence-transformers
 pip install llama-index-embeddings-instructor
 pip install llama-index-embeddings-huggingface
 pip install trafilatura
+pip install openai
+pip install llama-index-llms-ollama

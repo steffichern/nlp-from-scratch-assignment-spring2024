9515460bfb5bd573599e73d0db34ff04fe1b9900
Steffi Chern <90989714+steffichern@users.noreply.github.com>
2024-03-11 21:43:35 -0400

    txt files update

--- data/carnival_to_txt.py
+++ data/carnival_to_txt.py
@@ -10,6 +10,12 @@ def fetch_and_parse(url):
         content = html_file.read()
 
     soup = BeautifulSoup(content, 'html.parser')
+    text = [string.strip() for string in soup.stripped_strings]
+
+    # Join the text elements into a single string
+    all_text = '\n'.join(text)
+
+    '''
     # Remove scripts and styles
     for script_or_style in soup(["script", "style"]):
         script_or_style.decompose()
@@ -22,23 +28,26 @@ def fetch_and_parse(url):
     text_lines = []
 
     # Handle <h2> and <p> specifically to insert newlines
-    for elem in soup.find_all(['h2', 'p', 'span']):
+    for elem in soup.find_all(['h2', 'p', 'span', 'div', 'title']):
+        # Check if the tag is a div with a class attribute or any other specified condition
+        if elem.name == 'div' and 'class' in elem.attrs:
+            # Specific logic for div tags with a class, e.g., add a newline before the div content
+            text_lines.append('\n')  # Adjust this line as needed for your requirements
 
         # Strip the text and add a newline before it if it's not empty
         text = elem.get_text().strip()
         if text:
-            # Inserting a newline before each <h2> and <p> content except the first
             if text_lines: 
                 text_lines.append('\n')  
             text_lines.append(text)
 
     # Remove duplicates while preserving order
     text_lines = list(OrderedDict.fromkeys(text_lines))
-
+'''
     text_file_path = "./events/carnival.txt"
     with open(text_file_path, "w", encoding='utf-8') as file:
-        for line in text_lines:
-            file.write(f"{line}\n")
+        for line in all_text:
+            file.write(f"{line}")
 
 
 
